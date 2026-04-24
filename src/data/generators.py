3e26b2cbb0bb77675789fdd4a5b6079bb92b9654
merma1509 <118987634+merma1509@users.noreply.github.com>
2026-04-24 13:58:37 +0300

    Added Document Generator class

--- src/data/generators.py
+++
@@ -1,7 +0,0 @@
-"""
-Document generation components for traffic events retrieval system.
-"""
-
-from .document_generator import TrafficEventDocumentGenerator
-
-__all__ = ['TrafficEventDocumentGenerator']
